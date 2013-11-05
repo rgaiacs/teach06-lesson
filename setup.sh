f037cfe28acdbfb8da42e8bfff7a5a2ddffeead0
Raniere Silva <raniere@ime.unicamp.br>
2013-11-05 00:01:15 -0200

    Add bashrc and update setup
    * Add bashrc for dracula
    * Add bashrc for wolfman
    * Update setup.sh to handle the bashrc
    * Add option to clean in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-# This is a setup for my lesson.
+# For setup this lesson:
+#
+#     $ ./setup.sh
+#
+# For clean the repository for another lesson:
+#
+#     $ ./setup.sh clean
 
-# Create Mummy bare repository.
-cd .mummy
-git init -q
-git add *
-git commit -q -m 'Stable version'
-cd ..
-git clone -q --bare .mummy .mummy-bare
+if $(test "$1" = 'clean')
+then
+    # clean the repository
+    rm -rf .mummy/.git
+    rm -rf .mummy-bare
+    rm -rf dracula
+    rm -rf wolfman
+else
+    # Create Mummy bare repository.
+    cd .mummy
+    git init -q
+    git add *
+    git commit -q -m 'Stable version'
+    cd ..
+    git clone -q --bare .mummy .mummy-bare
 
-# Create Dracula repository
-git clone .mummy-bare dracula
+    # Create Dracula repository
+    git clone -q .mummy-bare dracula
+    cp .bashrc_dracula dracula/bashrc
+    cd dracula
+    git config alias.lga 'log --oneline --graph --decorate --all'
+    cd ..
 
-# Create Wolfman repository
-git clone .mummy-bare wolfman
+    # Create Wolfman repository
+    git clone -q .mummy-bare wolfman
+    cp .bashrc_wolfman wolfman/bashrc
+    cd wolfman
+    git config alias.lga 'log --oneline --graph --decorate --all'
+    cd ..
+fi
