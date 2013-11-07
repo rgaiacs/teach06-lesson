b9fdfb227f01ad248fcafb18cd01124fc441aec4
Raniere Silva <raniere@ime.unicamp.br>
2013-11-06 22:05:52 -0200

    Improve in setup.sh
    Add configuration for Mummy, Dracula and Wolfman.

--- setup.sh
+++ setup.sh
@@ -19,6 +19,8 @@ else
     # Create Mummy bare repository.
     cd .mummy
     git init -q
+    git config user.name Mummy
+    git config user.email mummy@software-carpentry.org
     git add *
     git commit -q -m 'Stable version'
     cd ..
@@ -28,13 +30,17 @@ else
     git clone -q .mummy-bare dracula
     cp .bashrc_dracula dracula/bashrc
     cd dracula
-    git config alias.lga 'log --oneline --graph --decorate --all'
+    git config user.name Dracula
+    git config user.email dracula@software-carpentry.org
+    git config alias.graph 'log --abbrev-commit --graph --decorate --all'
     cd ..
 
     # Create Wolfman repository
     git clone -q .mummy-bare wolfman
     cp .bashrc_wolfman wolfman/bashrc
     cd wolfman
-    git config alias.lga 'log --oneline --graph --decorate --all'
+    git config user.name Wolfman
+    git config user.email wolfman@software-carpentry.org
+    git config alias.graph 'log --abbrev-commit --graph --decorate --all'
     cd ..
 fi
